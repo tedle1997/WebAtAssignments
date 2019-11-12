8ad8699389c74b5abe320f624d417ee798160592
tedle1997 <tedle1997@yahoo.com>
2019-11-12 19:57:56 +0100

    commit changes

--- EX4/server.js
+++ EX4/server.js
@@ -24,139 +24,103 @@ const mimeType = {
 	html: "text/html"
 };
 
+var staticDIR = '/NodeStaticFiles/';
+
 routes['file'] = function (req, res) {
+
+	//--------Preprocess---------------------------------
 	if (req.method != 'GET') {
-		// Controlla che sia un GET
-		res.writeHead(405, {'Content-Type': 'text/html' }); // Errore 405
-		res.write("<h1>Error 405 - Non sei un GET :(</h1>");// Contenuto html
+		res.writeHead(405, {'Content-Type': 'text/html' });
+		res.write("<h1>Error 405 - NOT GET METHOD :(</h1>");
 		res.end();
 		return;
 	}
-
-	// http://localhost:3000/file/file%2001.txt [file 01.txt]
-	// --> file/file%2001.txt
-	let urlPath = url.parse(req.url).pathname
-	// --> NodeStaticFiles/file%2001.txt
-		.replace(/^(\/file\/)/, '/NodeStaticFiles/')
-		// ' ' <--> %20 [Spazi]
-		.replace(/\%20/g, ' ');;
-
-	// --> /home/user/someone/assignment4/NodeStaticFiles/file01.txt
+	//-----------------------------------------------------
+	let urlPath = url.parse(req.url).pathname.replace(/^(\/file\/)/, staticDIR);
 	urlPath = __dirname + urlPath;
 
 
+	//------Check-file-existence------------------------------
 	if (!fs.existsSync(urlPath)) {
-		// Il file non esiste
-		res.writeHead(404, { 'Content-Type': 'text/html' }); // Errore 404
-		res.write("<h1>Error 404 - File non trovato :(</h1>Che tristezza");// Contenuto html
+		res.writeHead(404, { 'Content-Type': 'text/html' });
+		res.write("<h1>ERROR 404 FILE NOT FOUND :(</h1>");
 		res.end();
 		return;
 	}
+	//---------------------------------------------------------
 
-	// Habemus file
-	let fileName = urlPath.split('/') // Crea array con i vari segmenti
-		.slice(-1)  // = .reverse()   // Gira al contrario l'array
-		[0];                          // Prendi il primo elemento dell'array girato
-
-	let extension = fileName.split('.') // Come per il path
-		.slice(-1)                      // Reverse
-		[0];                            // L'ultimo dell'originale
-
-
-	let type = mimeType[extension];        // Ottieni il mimeType relativo all'estensione
+	let fileName = urlPath.split('/').slice(-1)[0];
+	let extension = fileName.split('.').slice(-1)[0];
+	let type = mimeType[extension];
 	if (type == undefined) {
-		type = 'application/octet-stream'; // Generico / binario
+		type = 'application/octet-stream';
 	}
+
 
 	res.setHeader('Content-disposition', 'attachment; filename=\"' + fileName + '\"');
 	res.setHeader('Content-type', type);
-	fs.createReadStream(urlPath)
-		.pipe(res) // <- Butta il file dentro la risposta
-		.on('finish', () => {
-			// Quando abbiamo scaricato, chiudiamo la risposta
-			res.end();
-		});
-}
-
-// -------------------------------------------------
-// ESERCIZIO 3
+	fs.createReadStream(urlPath).pipe(res).on('finish', () => {
+		res.end();
+	});
+};
 
 routes['explore'] = function (req, res) {
+	//--------Preprocess---------------------------------
 	if (req.method != 'GET') {
-		// Controlla che sia un GET
-		res.writeHead(405, { 'Content-Type': 'text/html' }); // Errore 405
-		res.write("<h1>Error 405 - Non sei un GET :(</h1>");// Contenuto html
+		res.writeHead(405, {'Content-Type': 'text/html' });
+		res.write("<h1>Error 405 - NOT GET METHOD :(</h1>");
 		res.end();
 		return;
 	}
-
-	// http://localhost:3000/explore/level%201/foo.txt [level 1]
-	// --> /explore/level%201
-	let urlPath = url.parse(req.url).pathname
-	// --> NodeStaticFiles/level%201
-		.replace(/^(\/explore\/)/, '/NodeStaticFiles/')
-		// ' ' <--> %20 [Spazi]
-		.replace(/\%20/g, ' ');
-
-	// --> /home/user/someone/assignment4/NodeStaticFiles/level 1
+	//-----------------------------------------------------
+	let urlPath = url.parse(req.url).pathname.replace(/^(\/explore\/)/, '/NodeStaticFiles/').replace(/\%20/g, ' ');
 	urlPath = __dirname + urlPath;
+	let basePath = url.parse(req.url).pathname.replace(/^(\/explore)/, '');
 
-	// Percorso file
-	let basePath = url.parse(req.url).pathname // --> /explore/level%201
-		.replace(/^(\/explore)/, ''); // /level%201
+	console.log(basePath);
 
+	//------Check-file-existence------------------------------
 	if (!fs.existsSync(urlPath)) {
-		// Il file non esiste
-		res.writeHead(404, { 'Content-Type': 'text/html' }); // Errore 404
-		res.write("<h1>Error 404 - File non trovato :(</h1>Che tristezza");// Contenuto html
+		res.writeHead(404, { 'Content-Type': 'text/html' });
+		res.write("<h1>ERROR 404 FILE NOT FOUND</h1>");
 		res.end();
 		return;
 	}
+	//---------------------------------------------------------
 
-	let dirName = urlPath.split('/') // Crea array con i vari segmenti
-		.slice(-1)  // = .reverse()  // Gira al contrario l'array
-		[0];                         // Prendi il primo elemento dell'array girato
-
-
+	let dirName = urlPath.split('/').slice(-1)[0];
 	let htmlOut = '';
-	htmlOut += '<h1>' + dirName + '</h1>'; // Titolo in h1 con il nome della cartella
-	htmlOut += '<ul>'; // Iniziamo la lista
+	htmlOut += '<h1>' + dirName + '</h1>';
+	htmlOut += '<ul>';
 
-	let hostUrl = 'http://' + req.headers.host; // esempio: http://localhost:3000
-
-//NON MANCA UN IF?
-	// `.` -> la stessa cartella
-	htmlOut += '<li><a href=\"' + hostUrl + '/explore/' + basePath + '/.\">.</a></li>';
-	// `..` -> la cartella genitore
+	let hostUrl = 'http://' + req.headers.host;
+	htmlOut += '<li><a href=\"' + hostUrl + '/explore/' + basePath + '\">.</a></li>';
 	htmlOut += '<li><a href=\"' + hostUrl + '/explore/' + basePath + '/..\">..</a></li>';
 
 	fs.readdir(urlPath, (err, contents) => {
 		if (err) {
-			// Impossibile leggere la cartella
-			res.writeHead(500, { 'Content-Type': 'text/html' }); // Errore 500
-			res.write("<h1>Lettura cartella non riuscita</h1>"); // Contenuto html
+			res.writeHead(500, { 'Content-Type': 'text/html' });
+			res.write("<h1>ERROR 500</h1>");
 			res.end();
 			return;
 		}
 
 		contents.forEach((file) => {
-			let filePath = urlPath + '/' + file; // Percorso completo del file
-			let isDir = fs.lstatSync(filePath).isDirectory(); // Cartella o file?
+			let filePath = urlPath + '/' + file;
+			let isDir = fs.lstatSync(filePath).isDirectory();
 			let url = '';
 			if (isDir) {
-				// explore (cartella)
-				url = hostUrl + '/explore/' + basePath + file;
+				url = hostUrl + '/explore/' + basePath + '/' + file;
 			} else {
-				// file (download)
 				url = hostUrl + '/file/' + basePath + file;
 			}
 
 			htmlOut += '<li><a href=\"' + url + '\">' + file + '</a></li>';
 		});
 
-		htmlOut += '</ul>'; // Chiudi lista
+		htmlOut += '</ul>';
 		res.writeHead(200, { 'Content-Type': 'text/html' }); // 200 - OK
-		res.write(htmlOut); // Contenuto html
+		res.write(htmlOut);
 		res.end();
 	});
 }
@@ -186,15 +150,14 @@ function uploadPost(req, res) {
 	form.keepExtensions = true;
 
 	form.parse(req, (err, fields, files) => {
-		let origFile = files.file; // .file -> name="file" dal form
-		let destFilePath = uploadDir + origFile.name; // NodeStaticFiles/ilMioFile.txt
+		let origFile = files.file;
+		let destFilePath = uploadDir + origFile.name;
 
-		// Sposta il file da origFile.path a destFilePath
+
 		fs.rename(origFile.path, destFilePath, (err) => {
 			if (err) {
-				// Caricamento file fallito
-				res.writeHead(500, {'content-type': 'text/html'}); // Errore 500
-				res.write('<h1>Caricamento file fallito</h1>');    // Contenuto html
+				res.writeHead(500, {'content-type': 'text/html'});
+				res.write('<h1>Caricamento file fallito</h1>');
 				res.end();
 				return;
 			}
@@ -208,21 +171,12 @@ function uploadPost(req, res) {
 
 routes['upload'] = function (req, res) {
 	if (req.method == 'GET') {
-		// Mostra il form
 		uploadGet(req, res);
 	} else if (req.method == 'POST') {
-		// Carica il file
 		uploadPost(req, res);
 	}
 }
 
-// roba generale
-
-
-// Configure your routing table here...
-//routes['URL'] = function;
-
-// Main server handler
 function onRequest(req, res) {
 	const pathname = url.parse(req.url).pathname;
 	const uri = pathname.split('/', 3)[1]
@@ -235,4 +189,4 @@ function onRequest(req, res) {
 
 
 http.createServer(onRequest).listen(3000);
-console.log('Server started at localhost:3000')
+console.log('Server started at localhost:3000');
